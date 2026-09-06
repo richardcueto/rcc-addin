d0a9446fb95781181e2c07ec5b6f3d0232006f00
richardcueto <150373021+richardcueto@users.noreply.github.com>
2026-09-06 02:23:13 -0500

    guardlo9

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
 
-const certPath = path.join(os.homedir(), '.office-addin-dev-certs');
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+  const certPath = path.join(os.homedir(), '.office-addin-dev-certs');
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react()
-  ],
-  base: '/rcc-addin/',
-  server: {
-    port: 5173,
-    https: {
-      key: fs.readFileSync(path.join(certPath, 'localhost.key')),
-      cert: fs.readFileSync(path.join(certPath, 'localhost.crt')),
+  return {
+    base: '/rcc-addin/',
+    server: {
+      port: 5173,
+      // Solo intenta leer los certificados si estamos corriendo `npm run dev` en local
+      https: isDev ? {
+        key: fs.readFileSync(path.join(certPath, 'localhost.key')),
+        cert: fs.readFileSync(path.join(certPath, 'localhost.crt')),
+      } : false,
     },
-  },
-})
+  };
+});
